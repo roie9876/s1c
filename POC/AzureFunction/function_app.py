219670a7ed13ab06a3cefbc0e2e7ef2450ca6871
roie ben haim <roie9876@gmail.com>
2026-01-04 11:41:52 +0200

    update

--- POC/AzureFunction/function_app.py
+++ POC/AzureFunction/function_app.py
@@ -135,7 +135,8 @@ def download_launcher(req: func.HttpRequest) -> func.HttpResponse:
 #>
 
 param (
-    [string]$OverrideUser = ""
+    # [PoC Shortcut] Defaulting to the hardcoded Portal user so you don't have to type it every time
+    [string]$OverrideUser = "roie@mssp.com"
 )
 
 # --- CONFIGURATION ---
